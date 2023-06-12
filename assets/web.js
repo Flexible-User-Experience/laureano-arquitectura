2b67bea71b5dc5ae9cee13931ec815ca1194be56
David Romaní <david@flux.cat>
2023-06-12 16:04:15 +0200

    more dependencies updates

--- assets/web.js
+++ assets/web.js
@@ -8,8 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/web.scss';
 
-const jQuery = require('jquery');
-
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
@@ -17,7 +15,3 @@ require('bootstrap');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
-
-jQuery(document).ready(function() {
-    jQuery('[data-toggle="popover"]').popover();
-});
