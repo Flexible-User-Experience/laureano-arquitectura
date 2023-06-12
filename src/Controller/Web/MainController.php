4b6b12ef627e054963eea66efee9cde9d6454aeb
David Romaní <david@flux.cat>
2023-06-12 19:19:11 +0200

    add more translated routes

--- src/Controller/Web/MainController.php
+++ src/Controller/Web/MainController.php
@@ -8,7 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_web_homepage')]
+    #[Route(
+        path: '/',
+        name: 'app_web_homepage',
+    )]
     public function homepage(): Response
     {
         return $this->render('web/homepage.html.twig', [
@@ -16,7 +19,14 @@ class MainController extends AbstractController
         ]);
     }
 
-    #[Route('/projectes', name: 'app_web_projects_list')]
+    #[Route(
+        path: [
+            'ca' => '/projectes',
+            'es' => '/proyectos',
+            'en' => '/projects',
+        ],
+        name: 'app_web_projects_list',
+    )]
     public function projectsList(): Response
     {
         return $this->render('web/homepage.html.twig', [
@@ -24,8 +34,15 @@ class MainController extends AbstractController
         ]);
     }
 
-    #[Route('/projecte', name: 'app_web_project_detail')]
-    public function projectDetail(): Response
+    #[Route(
+        path: [
+            'ca' => '/projecte/{slug}',
+            'es' => '/proyecto/{slug}',
+            'en' => '/project/{slug}',
+        ],
+        name: 'app_web_project_detail',
+    )]
+    public function projectDetail(string $slug): Response
     {
         return $this->render('web/homepage.html.twig', [
             'controller_name' => 'MainController :: projectDetail',
@@ -33,6 +50,14 @@ class MainController extends AbstractController
     }
 
     #[Route('/contacte', name: 'app_web_contact')]
+    #[Route(
+        path: [
+            'ca' => '/contacte',
+            'es' => '/contacto',
+            'en' => '/contact',
+        ],
+        name: 'app_web_contact',
+    )]
     public function contact(): Response
     {
         return $this->render('web/homepage.html.twig', [
