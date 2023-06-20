1496db9aa57d7dae47cb85c5d65a0512eaa3437c
David Romaní <david@flux.cat>
2023-06-20 11:44:48 +0200

    replace GA tag

--- src/Manager/GoogleAnalyticsManager.php
+++ src/Manager/GoogleAnalyticsManager.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Google\Client as GoogleApiClient;
 use Google\Exception;
 use Google\Service\Analytics as GoogleAnalyticsDataService;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
@@ -15,6 +16,7 @@ class GoogleAnalyticsManager
 {
     public const GOOGLE_APIS_CREDENTIALS_FILENAME = 'google_apis_credentials.json';
 
+    private Security $ss;
     private UserRepository $ur;
     private GoogleApiClient $gac;
     private ?GoogleAnalyticsDataService $gads = null;
@@ -22,8 +24,9 @@ class GoogleAnalyticsManager
     /**
      * @throws Exception
      */
-    public function __construct(AssetsManager $am, ParameterBagInterface $pb, RouterInterface $rs, UserRepository $ur)
+    public function __construct(AssetsManager $am, ParameterBagInterface $pb, RouterInterface $rs, Security $ss, UserRepository $ur)
     {
+        $this->ss = $ss;
         $this->ur = $ur;
         $this->gac = new GoogleApiClient();
         $this->gac->setApplicationName($pb->get('project_web_title').' Google Analytics API Integration');
@@ -60,17 +63,46 @@ class GoogleAnalyticsManager
         return $this;
     }
 
-    public function getTotalVisitsAmount(): int
+    public function getTotalVisitsAmount(): array|string|int
     {
-//        $this->reFetchUserGoogleApiClientAccessToken();
-//        $data = $this->getGoogleAnalyticsDataService()->data_ga->get(
-//            'ga:12',
-//            '2023-06-15',
-//            '2023-06-19',
-//            'ga:sessions,ga:pageviews'
-//        );
-//        $data = $this->getGoogleAnalyticsDataService()->
-        return 0;
+        /** @var User $user */
+        $user = $this->ss->getUser();
+        if ($user->getGoogleCredentialsAccepted()) {
+            $this->reFetchUserGoogleApiClientAccessToken($user);
+            // Get the user's first view (profile) ID.
+            // Get the list of accounts for the authorized user.
+            $accounts = $this->getGoogleAnalyticsDataService()->management_accounts->listManagementAccounts();
+            if (count($accounts->getItems()) > 0) {
+                $items = $accounts->getItems();
+//                dd($items);
+                $firstAccountId = $items[3]->getId();
+                // Get the list of properties for the authorized user.
+                $properties = $this->getGoogleAnalyticsDataService()->management_webproperties->listManagementWebproperties($firstAccountId, [
+                    'max-results' => 50,
+                ]);
+                if (count($properties->getItems()) > 0) {
+                    $items = $properties->getItems();
+
+//                    dd($items);
+                } else {
+                    throw new Exception('No properties found for this user.');
+                }
+            } else {
+                throw new Exception('No accounts found for this user.');
+            }
+
+            $data = $this->getGoogleAnalyticsDataService()->data_ga->get(
+                'ga:5534643240',
+                '2023-06-15',
+                '2023-06-20',
+                'ga:sessions,ga:pageviews'
+            );
+//            dd($data);
+
+//            return $data->totalResults;
+        }
+
+        return -10;
     }
 
     public function reFetchUserGoogleApiClientAccessToken(User $user): void
