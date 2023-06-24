3eea8c61eb0d532a61d26fcc24569867c0ede6be
David Romaní <david@flux.cat>
2023-06-24 09:46:51 +0200

    test new RecurringMessage - 1

--- src/Schedule/Provider/DefaultScheduleProvider.php
+++ src/Schedule/Provider/DefaultScheduleProvider.php
@@ -11,14 +11,21 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 #[AsSchedule('default')]
 class DefaultScheduleProvider implements ScheduleProviderInterface
 {
+    /**
+     * @throws \Exception
+     */
     public function getSchedule(): Schedule
     {
         return (new Schedule())->add(
-//            RecurringMessage::every('1 day', new UpdateGoogleAnalyticsVisitsMessage(), from: '12:47')->withJitter(3600)
             RecurringMessage::every(
-                '120 seconds',
-                new UpdateGoogleAnalyticsVisitsMessage()
-            )->withJitter(4)
+                '1 day',
+                new UpdateGoogleAnalyticsVisitsMessage(),
+                from: new \DateTimeImmutable('12:47', new \DateTimeZone('Europe/Madrid'))
+            )->withJitter(3600)
+//            RecurringMessage::every(
+//                '120 seconds',
+//                new UpdateGoogleAnalyticsVisitsMessage()
+//            )
         );
     }
 }
