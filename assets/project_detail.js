478a2a5fd7024494efb3583608329b5b821c52c0
David Romaní <david@flux.cat>
2023-06-27 09:12:38 +0200

    add project detail BS carousel - 1

--- assets/project_detail.js
+++ assets/project_detail.js
@@ -1,8 +1,4 @@
-// require('bootstrap');
-
-console.log('Bootstrap Carousel!');
 document.addEventListener( 'DOMContentLoaded', function() {
-    console.log('addEventListener Bootstrap Carousel!!');
     const myCarouselElement = document.querySelector('#carousel');
     new bootstrap.Carousel(myCarouselElement, {
         interval: 2000,
