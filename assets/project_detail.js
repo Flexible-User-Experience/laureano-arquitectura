20a1caff9eea3c7a2fec01abced31f1fdbc1e6a5
David Romaní <david@flux.cat>
2023-06-27 09:53:17 +0200

    add project detail BS carousel - 2

--- assets/project_detail.js
+++ assets/project_detail.js
@@ -1,7 +1,7 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     const myCarouselElement = document.querySelector('#carousel');
     new bootstrap.Carousel(myCarouselElement, {
-        interval: 2000,
-        touch: false
+        interval: 4000,
+        touch: true
     });
 });
