ff1fd4971d86ce2775f9e80c79cbbad12fe4535b
David Romaní <david@flux.cat>
2023-06-27 11:47:46 +0200

    add project detail BS carousel - 4

--- assets/project_detail.js
+++ assets/project_detail.js
@@ -4,4 +4,9 @@ document.addEventListener( 'DOMContentLoaded', function() {
         interval: 4000,
         touch: true
     });
+    myCarouselElement.addEventListener('slide.bs.carousel', (event) => {
+        let elementChildren = document.querySelector('#indicators').children;
+        elementChildren.item(event.to).classList.remove('opacity-25');
+        elementChildren.item(event.from).classList.add('opacity-25');
+    });
 });
